23e4ebbe2674f5542e467218a6202e4ff4369eab
chenjianfang <173775295@qq.com>
2020-10-13 18:59:21 +0800

    修改配置

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -54,6 +54,13 @@ module.exports = {
                     name: 'images/[name].[hash:7].[ext]'
                 }
             }, {
+                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 1,
+                    name: 'media/[name].[hash:7].[ext]'
+                }
+            }, {
                 test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
                 loader: 'url-loader',
                 options: {
